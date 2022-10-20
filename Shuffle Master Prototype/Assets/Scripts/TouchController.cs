297d1cbb4bb6047b9b12d466e761431c8a60c277
Selin <selinmoralii@gmail.com>
2022-10-20 15:13:17 +0300

    Updated TouchController script.

--- Shuffle Master Prototype/Assets/Scripts/TouchController.cs
+++ Shuffle Master Prototype/Assets/Scripts/TouchController.cs
@@ -4,29 +4,27 @@ using UnityEngine;
 
 public class TouchController : MonoBehaviour
 {
-    private Vector2 _startTouchPosition, _endTouchPosition;
+    private Vector2 _startTouchPosition, _currentTouchPosition;
     private float _deltaPosition;
     private int _speedCoef;
 
     void Update()
     {
-        isClick(); //týklanýp týklanmadýðýný sürekli update de kontrol edeceðim.
+        isClick();
     }
                                                                               
     public void isClick()
     {
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
-            _startTouchPosition = Input.GetTouch(0).position; //dokunduðum andaki pozisyonumu deðiþkene eþitliyorum
+            _startTouchPosition = Input.GetTouch(0).position; 
         }
 
-        //parmaðýmla bir yere týkladýysam ve parmaðýmý sürüklediysem
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
         {
-            _endTouchPosition = Input.GetTouch(0).position;
-            _deltaPosition = _endTouchPosition.x - _startTouchPosition.x;
+            _currentTouchPosition = Input.GetTouch(0).position;
+            _deltaPosition = _currentTouchPosition.x - _startTouchPosition.x;
             _speedCoef = (int)Mathf.Abs(_deltaPosition / 100);
-            Debug.Log(_speedCoef);
         }
     } 
 }
