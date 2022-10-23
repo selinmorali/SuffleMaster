ca73ee1c89ff5da5f238630c8361984ccc8c373b
Selin <selinmoralii@gmail.com>
2022-10-23 23:44:58 +0300

    Gates and their mathematical operations added.

--- Shuffle Master Prototype/Assets/Scripts/Card.cs
+++ Shuffle Master Prototype/Assets/Scripts/Card.cs
@@ -6,6 +6,9 @@ public class Card : MonoBehaviour
 {
     public static Card Instance;
     public float CardHeight;
+    public string _operator;
+    public int value;
+
 
     void Awake()
     {
--- Shuffle Master Prototype/Assets/Scripts/TouchController.cs
+++ Shuffle Master Prototype/Assets/Scripts/TouchController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TouchController : MonoBehaviour
@@ -16,24 +18,30 @@ public class TouchController : MonoBehaviour
     {
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
-            _startTouchPosition = Input.GetTouch(0).position; 
+            _startTouchPosition = Input.GetTouch(0).position;
         }
 
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Stationary)
         {
             _currentTouchPosition = Input.GetTouch(0).position;
             _deltaPosition = _currentTouchPosition.x - _startTouchPosition.x;
             _speedCoef = (int)Mathf.Abs(_deltaPosition / 100);
-
-            if(_deltaPosition > 0)
+          
+            if (_deltaPosition > 0)
             {
-                leftHand.GetComponent<StackController>().RemoveCardFromDeck(1);
-                rightHand.GetComponent<StackController>().GetCardAndPlace(1);
+                if (leftHand.GetComponent<StackController>().currentStack.Count > 0)
+                {
+                    leftHand.GetComponent<StackController>().RemoveCardFromDeck(1);
+                    rightHand.GetComponent<StackController>().GetCardAndPlace(1);
+                }
             }
-            else if(_deltaPosition < 0)
+            else if (_deltaPosition < 0)
             {
-                rightHand.GetComponent<StackController>().RemoveCardFromDeck(1);
-                leftHand.GetComponent<StackController>().GetCardAndPlace(1);
+                if (rightHand.GetComponent<StackController>().currentStack.Count > 0)
+                {
+                    rightHand.GetComponent<StackController>().RemoveCardFromDeck(1);
+                    leftHand.GetComponent<StackController>().GetCardAndPlace(1);
+                }
             }
         }
     }
