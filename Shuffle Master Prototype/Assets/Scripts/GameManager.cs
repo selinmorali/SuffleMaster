7077d361cca559751bf6b66e33b156f655529d17
Selin <selinmoralii@gmail.com>
2022-10-25 20:26:05 +0300

    Improved animation quality and added game over panel.

---
+++ Shuffle Master Prototype/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject LeftHand, RightHand;
+    private int totalCardCount;
+    public GameObject GameOverPanel;
+    public bool isStarted = false;
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        GameOverPanel.SetActive(false);
+    }
+    void Update()
+    {
+        totalCardCount = LeftHand.GetComponent<StackController>().currentStack.Count + RightHand.GetComponent<StackController>().currentStack.Count;
+
+        if(isStarted && totalCardCount == 0)
+        {
+            GameOverPanel.SetActive(true);
+            StartCoroutine(nameof(CloseGame));
+        }
+    }
+
+    public IEnumerator CloseGame()
+    {
+        PlayerMove.Instance.speed = 0f;
+        yield return new WaitForSeconds(2);
+        UnityEditor.EditorApplication.isPlaying = false;  
+    }
+}
--- Shuffle Master Prototype/Assets/Scripts/Rotator.cs
+++ Shuffle Master Prototype/Assets/Scripts/Rotator.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-	public float speed = 3f;
+    public float speed = 3f;
 
-
-    // Update is called once per frame
     void Update()
     {
-		transform.Rotate(0f, 0f, speed * Time.deltaTime / 0.01f, Space.Self);
-	}
+        //Objenin kendi ekseninde donusunu saglar
+        transform.Rotate(0f, 0f, speed * Time.deltaTime / 0.01f, Space.Self);
+    }
 }
